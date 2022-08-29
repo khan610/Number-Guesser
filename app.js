734c813747429517d2e4d5ed5d1c30428523ccf0
Andrea Trajkovic <trajkovicandrea@gmail.com>
2022-08-29 05:51:42 +0200

    Update

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 /*
 GAME FUNCTION:
 - Player must guess a number between a min and max
-- Player gets a certain amount of guess
+- Player gets a certain amount of guesses
 - Notify player of guesses remaining
 - Notify the player of the correct answer if loose
 - Let player choose to play again
 */
 
-// Game value
+// Game values
 let min = 1,
   max = 10,
   winningNum = 2,
   guessesLeft = 3;
 
 // UI Elements
-const game = document.querySelector('game'),
+const game = document.querySelector('#game'),
   minNum = document.querySelector('.min-num'),
   maxNum = document.querySelector('.max-num'),
-  guessBtn = document.querySelector('.#guess-btn'),
-  guessInput = document.querySelector('.#guess-input'),
+  guessBtn = document.querySelector('#guess-btn'),
+  guessInput = document.querySelector('#guess-input'),
   message = document.querySelector('.message');
 
 // Assign UI min and max
